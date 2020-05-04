45f0d9b8424ef704ddb746888a46d196700886d1
kato_y <kato_y@auroraworks.co.jp>
2020-05-04 16:12:50 +0900

    第4章 URLディスパッチとURLconf - urls.py 分割後

--- config/urls.py
+++ config/urls.py
@@ -14,16 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
-from accounts import views as accounts_views
-from shop import views as shop_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('/', views.index, name='index'),
-    path('accounts/login/', accounts_views.login, name='accounts_login'),
-    path('shop/', shop_views.index, name='shop_index'),
-    path('shop/<int:book_id>', shop_views.detail, name='shop_detail'),
+    path('accounts/', include('accounts.urls')),
+    path('shop/', include('shop.urls')),
 ]
